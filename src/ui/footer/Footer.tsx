17ab6877c1f3e50ecfe71856c1791f412cf9ab8a
“Hanna <westphallucas1@outlook.com>
2023-01-12 11:32:46 -0300

    Ajustes footer

--- src/ui/footer/Footer.tsx
+++ src/ui/footer/Footer.tsx
@@ -1,10 +1,11 @@
 import { FaHeart } from 'react-icons/fa';
 import { SiTwitter } from 'react-icons/si';
+import './footer.css';
 
 export function Footer() {
     return (
-        <div style={{ backgroundColor: "rgba(30, 30, 30, 0.6)", height: "120px", display: "flex", flexDirection: "row", justifyContent: "space-between", alignItems: "end", padding: "12px" }}>
-            <span>Ordem Paranormal é do Cellbit</span>
+        <div className='footer'>
+            <small>Ordem Paranormal é do Cellbit</small>
             <small>
                 Made with <FaHeart color='red'/> by <a style={{ textDecoration: "none", color: "white" }} href='https://twitter.com/hannaBanannaOF' target={"_blank"} rel={"noreferrer"}>
                     <SiTwitter color='aqua'/>@hannaBanannaOF
