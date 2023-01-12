0455f778b0fd3ee52c9bb1331f67d5bb814cefcb
“Hanna <westphallucas1@outlook.com>
2023-01-12 16:09:52 -0300

    GTM

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import TagManager from 'react-gtm-module';
+
+const tagManagerArgs = {
+    gtmId: 'GTM-M8MSBFP'
+}
+
+TagManager.initialize(tagManagerArgs)
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
